30fa1d0a7b34161bcd274f9ad055720dcc86149e
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-02-03 20:43:13 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Bookkeeper!</h1>
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
+    </nav>
   )
 }
 
